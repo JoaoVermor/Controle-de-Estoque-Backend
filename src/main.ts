a64482ca025aac4c488011d494579269a7ca844b
JoaoVermor <joaopaulovermor01@gmail.com>
2025-06-11 11:52:33 -0300

    v1.0

--- src/main.ts
+++ src/main.ts
@@ -4,7 +4,7 @@ import { AppModule } from './app.module';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors({
-    origin: '*',
+    origin: ['*', "https://falcon6rocketdesign.vercel.app/"],
     credentials: true,
   });
   await app.listen(3000);
