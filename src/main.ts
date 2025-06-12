6594d25a689497a6adff46d90624e028462b88f0
JoaoVermor <joaopaulovermor01@gmail.com>
2025-06-12 10:23:56 -0300

    v1.0

--- src/main.ts
+++ src/main.ts
@@ -3,10 +3,18 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  
   app.enableCors({
-    origin: ['*', "https://falcon6rocketdesign.vercel.app/"],
+    origin: [
+      'https://falcon6rocketdesign.vercel.app',
+      'http://localhost:3000', // para desenvolvimento local
+      'http://localhost:3001'  // caso use outra porta
+    ],
+    methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
+    allowedHeaders: ['Content-Type', 'Authorization'],
     credentials: true,
   });
+  
   await app.listen(3000);
 }
 bootstrap();
